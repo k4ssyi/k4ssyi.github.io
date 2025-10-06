a79a19c17ac8fa7822bbf3350d64ddbcffd3b99c
k4ssyi <kasikasi00@gmail.com>
2025-10-07 03:30:58 +0900

    Updates

---
+++ _next/static/chunks/app/page-ada438789745ebfc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{3212:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9299,23))}},e=>{e.O(0,[299,800,304,358],()=>e(e.s=3212)),_N_E=e.O()}]);
---
+++ _next/static/chunks/pages/_app-7f75f437bed57940.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{1315:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(7871)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(1315),n(3095))),_N_E=_.O()}]);
